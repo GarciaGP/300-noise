429a5fd0a21da827319df7c3eeb1dff008beb469
GarciaGP <gabriel.garp@outlook.com>
2024-08-07 22:18:32 -0300

    apply responsiveness to about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,34 +152,29 @@ export default function Home() {
         </div>
       </section>
       <section className="about dark-text" id="about">
-        <div className="section-title about-title">
-          <h3>QUEM SOMOS</h3>
-        </div>
-        <div className="text-area">
-          <p>A 300Noise é um estúdio de pesquisa e comunicação focado em cultura.</p>
+        <div className="about-info-wrapper">
+          <div className="section-title about-title">
+            <h3>QUEM SOMOS</h3>
+          </div>
+          <div className="text-area">
+            <p>A 300Noise é um estúdio de pesquisa e comunicação focado em cultura.</p>
 
-          <p>Nossa equipe é formada por cientistas sociais, profissionais da comunicação, história, geografia
-            e artes visuais. A partir dessa diversidade, criamos uma fórmula bem particular de analisar a
-            música, a cultura e as pessoas.</p>
+            <p>Nossa equipe é formada por cientistas sociais, profissionais da comunicação, história, geografia
+              e artes visuais. A partir dessa diversidade, criamos uma fórmula bem particular de analisar a
+              música, a cultura e as pessoas.</p>
 
-          <p>A 300Noise se apoia em números, redes sociais, teoria da comunicação, história, charts,
-            tendências e tudo mais que for útil para analisar a realidade. Trabalhamos para difundir
-            conhecimento sobre música e cultura, produzindo conteúdo de qualidade e sem amarras. Produzimos
-            análises de dados, consultorias, pesquisas, estratégias de comunicação e curadoria... ou o que
-            mais você e seu projeto precisarem.</p>
-
+            <p>A 300Noise se apoia em números, redes sociais, teoria da comunicação, história, charts,
+              tendências e tudo mais que for útil para analisar a realidade. Trabalhamos para difundir
+              conhecimento sobre música e cultura, produzindo conteúdo de qualidade e sem amarras. Produzimos
+              análises de dados, consultorias, pesquisas, estratégias de comunicação e curadoria... ou o que
+              mais você e seu projeto precisarem.</p>
+          </div>
         </div>
         <div className="about-cards-wrapper">
           <div className="about-card text-italic">
             <div className="about-card-outline"></div>
             <div className="icon">
               <img src="/consultoria.png" alt="consultoria" />
-              {/* <Image
-              fill={false}
-                className="icon-img"
-                src="/consultoria.png"
-                alt="consultoria"
-              /> */}
             </div>
             <div className="card-text">
               <p>CONSULTORIA</p>
@@ -253,24 +248,7 @@ export default function Home() {
           <div><p>Recebemos seu e-mail!</p>
             <p>Responderemos assim que possível! </p></div>
         }
-        {/* <div className="form-area">
-          <div className="form-wrapper">
-            <form action="post">
-              <input type="text" name="name" id="" placeholder="Nome" />
-              <input type="text" name="email" id="" placeholder="Email" />
-              <textarea rows={8} name="suggestions" id=""
-                placeholder="O que tem para nos dizer?"></textarea>
-              <button type="submit">ENVIAR</button>
-            </form>
-          </div>
-        </div> */}
       </section>
-
-      {/* <div id="news" className="news">
-        <div className="modal-content">
-        <img src="assets/img/png_loja.png" alt="Imagem Loja" />
-        </div>
-      </div> */}
 
     </div>
 
